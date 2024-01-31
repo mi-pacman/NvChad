8d4cb61f2e248dcdac0e0f3dcf020d8a37102315
Your Name <you@example.com>
2024-01-31 13:23:30 +0930

    removed custom.mappings

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,4 @@
-
----@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'onedark'}
- M.plugins = "custom.plugins"
- M.mappings = require "custom.mappings"
- return M
+local M = {}
+M.ui = {theme = 'onedark'}
+M.plugins = "custom.plugins"
+return M
