9c3550a99f517cfbb666ee5b4b18378467761f66
Your Name <you@example.com>
2024-01-31 12:40:52 +0930

    adding custom directory to git repo

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,12 @@
+local plugins = {
+  {
+  "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    lazy = false,
+  },
+}
+
+return plugins
